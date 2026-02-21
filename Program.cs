87c7eb3292492952ddc24d61cdc9eca45bb1a19d
arbys27 <99326044+dracs0@users.noreply.github.com>
2026-02-21 17:59:43 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sched;
+            string subject;
+
+            Console.WriteLine("Subject and Scheduling");
+            Console.WriteLine(" ");
+
+            var subjects = new string[4];
+            subjects[0] = "OOP";
+            subjects[1] = "Info Management";
+            subjects[2] = "Network Administration";
+            subjects[3] = "Integrative Programming";
+            subjects[4] = "Free Elective";
+
+            Console.WriteLine("Enter the time: ");
+            string sched = Console.ReadLine();
+        }
+    }
+}
