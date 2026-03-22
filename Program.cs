37e67a7b8b5c9cea70957fca0dbedc9f9cacc782
baschicosci-ux <baschicosci@gmail.com>
2026-03-23 05:41:41 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -3,28 +3,63 @@
     internal class Program
     {
         static void Main()
+    {
+        int choice;
+        Console.WriteLine("1. Input Subjects");
+        Console.WriteLine("2. Input Schedule");
+        Console.WriteLine("3. Exit");
+        Console.WriteLine(" ");
+        Console.Write("Enter your choice: ");
+        choice = Convert.ToInt32(Console.ReadLine());
+
+        switch (choice)
         {
-            string[] subjects = new string[0];
-
-            Console.WriteLine("Enter subjects (type 'done' if youre finish inputing the subjects):");
-
-            while (true)
-            {
-                Console.Write("> ");
-                string input = Console.ReadLine();
-
-                if (input.ToLower() == "done")
-                    break;
-                //Turo ni sir well.. yubg array.resize is magdedepende yung size nang array sa dami nang input nang user
-                Array.Resize(ref subjects, subjects.Length + 1);
-                subjects[subjects.Length - 1] = input;
-            }
-            //gagamitin mo yung foreach each para yung loop konis naka depende sa kada input nang user...
-            Console.WriteLine("\nSubjects:");
-            foreach (var subject in subjects)
-            {
-                Console.WriteLine(subject);
-            }
+            case 1:
+                InputSubjects();
+                break;
+            case 2:
+                InputSchedule();
+                break;
+            case 3:
+                Environment.Exit(0);
+                break;
+            default:
+                Console.WriteLine("Invalid choice");
+                break;
         }
     }
-}
+
+    static void InputSubjects()
+    {
+        string[] subjects = new string[0];
+        Console.WriteLine("Enter subjects (type 'done' if you're finish inputing the subjects):");
+        while (true)
+        {
+            Console.Write("• ");
+            string input = Console.ReadLine();
+            if (input.ToLower() == "done") break;
+            Array.Resize(ref subjects, subjects.Length + 1);
+            subjects[subjects.Length - 1] = input;
+        }
+
+        Console.WriteLine("\nSubjects:");
+        foreach (var subject in subjects)
+        {
+            Console.WriteLine(subject);
+        }
+    }
+
+    static void InputSchedule()
+    {
+        Console.Write("Enter day (Example: Monday): ");
+        string day = Console.ReadLine();
+
+        Console.Write($"Enter start time for {day} (Example: 7): ");
+        int startTime = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write($"Enter end time for {day} (Example: 12): ");
+        int endTime = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"\nSchedule for {day}: {startTime}:00 - {endTime}:00");
+    }
+    }
